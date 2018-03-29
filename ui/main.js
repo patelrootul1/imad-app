a9ae6eae09742f213ba0a067cffd808c677aa6c0
Rootul Patel <36380953+patelrootul1@users.noreply.github.com>
2018-03-29 17:28:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+var nameinput=document.getlementbyid('name');
+var name = nameinput.value;
+var submit = document.getelementbyid('submit_btn');
+submit.onclick = function(){
+  var names=['name1','name2','name3','name4']; 
+  var list='';
+  for(var i=0;i<names.length;i++){
+      list += '<li>' + names[i] + '</li>'; 
+  }
+  var ul=document.getelementbyid('namelist');
+  ul.innerHTML = list;
+  
+  
+};
